fe9a4f1e8929af2242c8cb85b4782acd42ff5a2f
Xhacker Liu <liu.dongyuan@gmail.com>
2014-03-25 18:13:50 -0700

    Show/hide web view.

--- menubar-webkit/LDYAppDelegate.m
+++ menubar-webkit/LDYAppDelegate.m
@@ -7,10 +7,12 @@
 //
 
 #import "LDYAppDelegate.h"
+#import <WebKit/WebKit.h>
 
 @interface LDYAppDelegate ()
 
 @property NSStatusItem *statusItem;
+@property (weak) IBOutlet WebView *webView;
 
 @end
 
@@ -21,9 +23,18 @@
     NSStatusBar *bar = [NSStatusBar systemStatusBar];
 
     self.statusItem = [bar statusItemWithLength:NSVariableStatusItemLength];
-
     self.statusItem.title = @"Xhacker";
     self.statusItem.highlightMode = YES;
+    [self.statusItem setAction:@selector(statusItemClicked)];
+
+    self.window.level = NSStatusWindowLevel;
+
+    self.webView.mainFrameURL = @"http://xhacker.im";
+}
+
+- (void)statusItemClicked
+{
+    self.window.isVisible = !self.window.isVisible;
 }
 
 @end
